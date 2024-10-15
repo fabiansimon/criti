e168fa174ea982e7d28de72bd6444282f8c33200
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2024-10-15 15:41:11 +0200

    simple navbar

---
+++ src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Home11Icon } from "hugeicons-react";
+import { usePathname, useRouter } from "next/navigation";
+import Text from "~/components/typography/text";
+import { route, ROUTES } from "~/constants/routes";
+import { cn } from "~/lib/utils";
+
+interface NavOption {
+  title: string;
+  icon: React.ReactNode;
+  route: string;
+  onClick: () => void;
+}
+
+export default function NavBar() {
+  const router = useRouter();
+  const path = usePathname();
+
+  const options: NavOption[] = [
+    {
+      icon: <Home11Icon size={18} className="text-neutral-700" />,
+      title: "Home",
+      route: ROUTES.home,
+      onClick: () => router.push(route(ROUTES.home)),
+    },
+  ];
+
+  return (
+    <div className="fixed left-0 right-0 top-0 flex h-12 items-center justify-between border-b border-b-neutral-200 bg-white">
+      <div className="mx-auto flex space-x-2">
+        {options.map((option, index) => (
+          <NavItem
+            active={path.includes(option.route)}
+            key={index}
+            option={option}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+interface NavItemProps {
+  option: NavOption;
+  active: boolean;
+}
+
+function NavItem({ option, active }: NavItemProps) {
+  const { icon, title, onClick } = option;
+  return (
+    <div
+      onClick={onClick}
+      className="flex cursor-pointer flex-col items-center rounded-md px-3 py-1 hover:bg-neutral-100"
+    >
+      <div className="flex items-center space-x-2">
+        {icon}
+        <Text.Body>{title}</Text.Body>
+      </div>
+      <div
+        className={cn(
+          "h-[1px] w-full rounded-md bg-neutral-300",
+          !active && "opacity-0",
+        )}
+      />
+    </div>
+  );
+}
